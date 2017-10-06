12d7a4741601d5b7813c91ab6aabaa82ea6a099c
muhammadilhamseptiawan <muhammad.septiawan@students.amikom.ac.id>
2017-10-06 10:25:30 +0700

    login

--- Hmm-app/app/src/main/java/com/ilhamseptiawan/hmm_app/SplashActivity.java
+++ Hmm-app/app/src/main/java/com/ilhamseptiawan/hmm_app/SplashActivity.java
@@ -1,13 +1,37 @@
 package com.ilhamseptiawan.hmm_app;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+
     }
 }
