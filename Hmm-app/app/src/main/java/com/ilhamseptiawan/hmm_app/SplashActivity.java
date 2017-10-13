b2c60d9e3e7f19faa5519693eff0fb0a6af4fa74
muhammadilhamseptiawan <muhammad.septiawan@students.amikom.ac.id>
2017-10-13 10:42:37 +0700

    amikom

--- Hmm-app/app/src/main/java/com/ilhamseptiawan/hmm_app/SplashActivity.java
+++ Hmm-app/app/src/main/java/com/ilhamseptiawan/hmm_app/SplashActivity.java
@@ -1,37 +1,37 @@
-package com.ilhamseptiawan.hmm_app;
+package net.ariflaksito.amikomapp;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class SplashActivity extends Activity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splash);
 
-        Thread timerThread = new Thread(){
+        Thread timer = new Thread(){
             public void run(){
                 try{
                     sleep(3000);
-                }catch(InterruptedException e){
+                }catch (InterruptedException e){
                     e.printStackTrace();
-                }finally{
-                    Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
-                    startActivity(intent);
+                }finally {
+                    Intent i = new Intent(getBaseContext(), LoginActivity.class);
+                    startActivity(i);
                     finish();
                 }
             }
         };
-        timerThread.start();
-    }
 
-    @Override
-    protected void onPause() {
-        // TODO Auto-generated method stub
-        super.onPause();
-        finish();
-
+        timer.start();
     }
 }
